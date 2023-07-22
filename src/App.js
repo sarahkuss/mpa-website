a0a32896778561bd34b962690050a21394e1fc07
Sarah Kuss <sarah.kuss33@gmail.com>
2023-07-22 11:45:59 -0400

    setup routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Homepage from './pages/Homepage';
+import About from './pages/About';
+import Services from './pages/Services';
+import Benefits from './pages/Benefits';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/benefits' element={<Benefits />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
