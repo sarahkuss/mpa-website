a940e007136537be7b6d36ac7e1eebfe16997122
Sarah Kuss <sarah.kuss33@gmail.com>
2023-07-29 19:59:38 -0400

    added video, quote, and button

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,40 @@
+import { Container, Row, Col } from "react-bootstrap"
+import { useNavigate } from "react-router-dom"
+import "../styles/homepage.css"
+
+
+
 export default function Homepage () {
+  const videoUri = process.env.PUBLIC_URL + '/images/walking.mp4'
+  const navigate = useNavigate()
+  const navigateToBenefits = () => {
+    navigate('/benefits')
+  }
   return (
-    <></>
+    <>
+      <Container fluid>
+        <Row d-flex justify-content-center>
+          <Col xs={11}
+            md={{offset:6, span:6}}
+            lg={{offset:7, span:4}}>
+            <div className="video-container">
+              <video autoPlay muted loop>
+                <source src={videoUri} type="video/mp4" />
+              </video>
+            </div>
+
+            <div className="video-content">
+              <p>Sometimes the smallest step <br />
+                In the right direction <br />
+                ends up being the biggest <br />
+                step of your life. <br />
+                Tiptoe if you must, but <br />
+                </p>
+              <button onClick={navigateToBenefits} className="step-button">Take a step</button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
   )
 }
