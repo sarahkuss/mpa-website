de01ec444e124285ea43a5e77806832e270c72fb
Sarah Kuss <sarah.kuss33@gmail.com>
2023-07-30 22:06:02 -0400

    started formatting benefits

--- src/pages/Benefits.jsx
+++ src/pages/Benefits.jsx
@@ -1,5 +1,25 @@
+import { Container, Row, Col } from "react-bootstrap";
+import '../styles/benefits.css'
+
 export default function Benefits () {
   return (
-    <></>
+    <>
+    <Container className="bg-primary" fluid>
+      <Row><Col className="benefits-title"><h1>Benefits</h1></Col></Row>
+      <Row >
+        <Col className="bg-warning">sdfs</Col>
+        <Col className="bg-danger"></Col>
+      </Row>
+      
+      <Row>
+        <Col className="bg-success">lskjdfsl</Col>
+      </Row>
+      
+      <Row>
+        <Col className="bg-danger">fsd</Col>
+        <Col className="bg-warning">sdfs</Col>
+      </Row>
+    </Container>
+    </>
   )
 }
