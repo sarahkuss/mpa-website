d8f0d36de7a9932190bc46ee6f868360d1145bea
Sarah Kuss <sarah.kuss33@gmail.com>
2023-08-08 23:26:45 -0400

    gave contact page background color

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,15 @@
+import { Col, Container, Row } from "react-bootstrap";
+import '../styles/contact.css'
+
+
 export default function Contact () {
   return (
-    <></>
+    <>
+      <Container className="contact-container" fluid>
+        <Row>
+          <Col></Col>
+        </Row>
+      </Container>
+    </>
   )
 }
